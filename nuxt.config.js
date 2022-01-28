0d822632d3e092585f42bc5b9b89b6660c13e879
Niranjanck <niranjankkv1999@gmail.com>
2022-01-28 18:47:00 +0530

    home

---
+++ nuxt.config.js
@@ -0,0 +1,85 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - farmers',
+    title: 'farmers',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+     [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyCrtu7XGZ4Y_S4k-Yr4l2oae0O7uZgHxPg",
+          authDomain: "farmer-c86e6.firebaseapp.com",
+          projectId: "farmer-c86e6",
+          storageBucket: "farmer-c86e6.appspot.com",
+          messagingSenderId: "641071961042",
+          appId: "1:641071961042:web:084798ad5baa70502eae0b",
+          measurementId: "G-ZJVYLHWR5W"
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+      }
+    ]
+  ],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="main">
+    <div class="leftSideContainer container">
+      <!-- logo -->
+      <div class="image"></div>
+      <!-- heading -->
+      <h2 class="farm">Farmer</h2>
+      <!-- buttons -->
+      <v-btn
+        class="navBtn"
+        v-for="(button,i) in navbar"
+        :key="i"
+        >{{button.value}}
+      </v-btn>
+      <signUpPopUp />
+    </div>
+    <div class="rightSideContainer container">
+      <!-- product list -->
+      <div>
+        <v-card
+          v-for="(card , i) in cards"
+          :key="i"
+          class="mx-auto productCard"
+        >
+          <v-img
+            class="white--text align-end"
+            height="500px"
+            :src="card.src"
+          >
+          </v-img>
+
+          <v-card-title class="pb-0">
+            {{card.name}}
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>1 KG Price = {{card.price}}Rs</div>
+            <div>{{card.place}}</div>
+
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              class="shopBtn"
+              text
+            >
+              Shop
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.shopBtn{
+  background-color: #ff8800;
+  color: rgb(255, 255, 255);
+}
+.productCard{
+  width: 80%;
+  margin: auto;
+  margin-top: 30px;
+}
+.farm{
+  text-align: center;
+  color: rgb(255, 92, 51);
+}
+.navBtn{
+  background-color: tomato;
+  width: 100%;
+  margin-top: 10px;
+}
+.image{
+  background-image: url('https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg');
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 200px;
+}
+
+.main{
+  display: table;
+  width: 100%;
+  height: 100vh;
+}
+.leftSideContainer{
+  position: fixed;
+  height: 100vh;
+  width: 20%;
+  background-color: #e5e5e5;
+}
+.container{
+  display: table-cell;
+  }
+.rightSideContainer{
+  justify-content: center;
+  justify-items: center;
+  width: 80%;
+  background-color: rgb(255, 255, 255);
+}
+</style>
+<script>
+import SignUpPopUp from '/home/ck/project/Linsa/farmer/farmers/components/signUpPopUp.vue'
+
+
+export default {
+  coponents: {
+    SignUpPopUp,
+  },
+  data() {
+    return {
+      navbar: [
+        {value: 'Home', path: '/'},
+        {value: 'About', path: '/about'},
+        {value: 'Contact', path: '/contact'},
+        {value: 'SignIn/SignOut', path: '/contact'},
+      ],
+      cards: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            name:"Carrot",
+            place:"Malappuram ",
+            price:"200",
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            name:"Tomato",
+            place:"Kozhikode",
+            price:"200",
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            name:"Beans",
+            place:"Thrissur",
+            price:"200",
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            name:"Chilli",
+            place:"Kannur",
+            price:"200",
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            name:"Bitroot",
+            place:"Wayanad",
+            price:"200",
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            name:"Potato",
+            place:"Kozhikode",
+            price:"200",
+          },
+        ],
+        dialog: false,
+    }
+  },
+}
+</script>
